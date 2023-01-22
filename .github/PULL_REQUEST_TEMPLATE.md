3dc42ab4e4bd3e91aca649d65ff8b05b966ebf48
jose rangel <jl.rangel20@hotmail.com>
2023-01-21 22:23:37 -0500

    frist commit

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,13 @@
+## Trello Ticket
+
+- [INSERT NAME HERE](INSERT LINK HERE)
+
+## Description
+
+AS:
+I Want:
+To:
+
+## Evidence:
+
+(Put screenshots/video/gifs here)
---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.use(cors());
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, () => {
+	console.log('servidor conectado');
+});
