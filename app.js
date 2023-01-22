2d10c7853ed83eeba9c23dc01a3778ef5a6f2121
jose rangel <jl.rangel20@hotmail.com>
2023-01-21 23:08:26 -0500

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
+//const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
@@ -14,20 +14,20 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-	next(createError(404));
+  next(createError(404));
 });
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
-	const status = err.statusCode || 500;
-	const message = err.message || err;
-	res.status(status).json({ message });
+  const status = err.statusCode || 500;
+  const message = err.message || err;
+  res.status(status).json({ message });
 });
 
 app.set('port', config.port);
